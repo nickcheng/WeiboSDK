63f6240e8164393e79d48da55b53846722dc0e28
nickcheng <n@nickcheng.com>
2013-01-16 18:19:33 +0800

    Put error domain setting to WeiboConfig

--- sdk/WeiboEngine/WeiboConfig.h
+++ sdk/WeiboEngine/WeiboConfig.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2012年 idfsoft. All rights reserved.
 //
 
-#define kWeiboAuthorizeURL          @"https://api.weibo.com/oauth2/authorize"
-#define kWeiboAccessTokenURL        @"https://api.weibo.com/oauth2/access_token"
-#define kWeiboAPIBaseUrl            @"https://api.weibo.com/2/"
-
+#define kWeiboAuthorizeURL @"https://api.weibo.com/oauth2/authorize"
+#define kWeiboAccessTokenURL @"https://api.weibo.com/oauth2/access_token"
+#define kWeiboAPIBaseUrl @"https://api.weibo.com/2/"
+#define kWeiboOAuth2ErrorDomain @"com.zhiweibo.OAuth2"
 
