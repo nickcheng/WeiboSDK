24bdd12f8761f6b31b25c7b2f74781f311ae0160
nickcheng <n@nickcheng.com>
2013-01-18 11:48:28 +0800

    Added upload image file name.

--- sdk/WeiboEngine/WeiboConfig.h
+++ sdk/WeiboEngine/WeiboConfig.h
@@ -12,3 +12,4 @@
 #define kWeiboOAuth2ErrorDomain @"com.zhiweibo.OAuth2"
 #define kWeiboAPIErrorDomain @"com.zhiweibo.api"
 #define kWeiboUserAgent @"ZhiWeibo"
+#define kWeiboUploadImageName @"zhiweibo.png"
